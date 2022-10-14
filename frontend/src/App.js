b59d3cd9a4ce44a5201939f76e051c69a4da1b1d
sebastianiv21 <seluti98@gmail.com>
2022-10-13 20:51:09 -0500

    Section 2: Starting the Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,30 @@
+import { Container } from 'react-bootstrap'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
+
 const App = () => {
   return (
-    <>
-      <h1>Welcome to ProShop</h1>
-    </>
+    <Router>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Routes>
+            <Route
+              path='/'
+              element={<HomeScreen />}
+            />
+            <Route
+              path='/product/:id'
+              element={<ProductScreen />}
+            />
+          </Routes>
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   )
 }
 
